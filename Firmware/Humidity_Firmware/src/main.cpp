0bb5449d6b5ec5ffc1eac6becd6d6efb387ed457
Pramathesh V <pramatheshv2904@gmail.com>
2026-06-04 12:05:53 +0530

    Updated the code and changed the board

--- Firmware/Humidity_Firmware/src/main.cpp
+++ Firmware/Humidity_Firmware/src/main.cpp
@@ -1,18 +1,40 @@
-#include <Arduino.h>
+#include "DHT.h"
+#define DHTPIN 4  
+#define DHTTYPE DHT22   
+DHT dht(DHTPIN, DHTTYPE);
 
-// put function declarations here:
-int myFunction(int, int);
+float avg_humidity = 0.0;
+float avg_temperature = 0.0;
+int count = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("DHT22 Sensor Data");
+
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  delay(500);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor");
+    return;
+  }
+
+  else{
+    count++;
+    avg_humidity = ((avg_humidity * (count - 1)) + humidity) / count;
+    avg_temperature = ((avg_temperature * (count - 1)) + temperature) / count;
+  }
+
+  Serial.print("Relative Humidity: ");
+  Serial.print(avg_humidity);
+  Serial.print("%  |  ");
+  Serial.print("Temperature: ");
+  Serial.print(avg_temperature);
+  Serial.println("°C");
 }
